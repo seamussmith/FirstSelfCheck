31eb42d96cf8d816ad78593bd5d3a97ab3321805
seamussmith <student.141504@worcesterschools.net>
2021-09-14 07:11:37 -0400

    Start writing prime sieve

--- src/main/App.java
+++ src/main/App.java
@@ -1,9 +1,23 @@
 package main;
 
+import java.util.*;
+import java.util.stream.IntStream;
+
 public class App
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        System.out.println(primeSieve(100));
+    }
+    static int[] primeSieve(int upTo)
+    {
+        var primes = new HashSet<Integer>();
+        var max = upTo - (upTo % 2 == 0 ? 1 : 0);
+        var nums = IntStream.rangeClosed(2, upTo)
+            .boxed()
+            .filter(i -> i % 2 != 0 || i == 2)
+            .toList();
+        System.out.println(nums);
+        return null;
     }
 }
